87d65e0ca2f06ec60c461d800449cfbe6329e563
Prabir Shrestha <mail@prabir.me>
2021-07-31 14:01:12 -0700

    add stdin support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use colored::*;
 use structopt::StructOpt;
 
@@ -10,11 +12,21 @@ struct Options {
     #[structopt(short = "d", long = "dead")]
     /// Make the cow appear dead
     dead: bool,
+
+    #[structopt(short = "i", long = "stdin")]
+    /// Read the message from STDIN instead of the argument
+    stdin: bool,
 }
 
 fn main() {
     let options = Options::from_args();
-    let message = options.message;
+
+    let mut message = String::new();
+    if options.stdin {
+        std::io::stdin().read_to_string(&mut message).unwrap();
+    } else {
+        message = options.message;
+    }
 
     if message.to_lowercase() == "woof" {
         eprintln!("A cow shouldn't bark like a dog.");
