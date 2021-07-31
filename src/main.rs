a12d84843531e5330edd586565977a20da817a8c
Prabir Shrestha <mail@prabir.me>
2021-07-31 13:47:00 -0700

    add -d/--dead support

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,21 @@ struct Options {
     #[structopt(default_value = "Moo!")]
     /// What does the cow say?
     message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cow appear dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::from_args();
     let message = options.message;
 
+    let eye = if options.dead { "x" } else { "o" };
+
     println!("{}", message);
     println!("   \\   ^__^");
-    println!("     \\  (oo)\\______");
+    println!("     \\  ({}{})\\______", eye, eye);
     println!("        (__)\\      )\\/\\");
     println!("           ||----w |");
     println!("           ||     ||");
